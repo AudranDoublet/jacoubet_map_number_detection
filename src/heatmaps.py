9e9d4eb7096e0452943c20ec1e6d6771bc581c74
Audran Doublet <audran.doublet@lse.epita.fr>
2020-07-03 04:12:18 +0200

    heatmaps: refacto

--- src/heatmaps.py
+++ src/heatmaps.py
@@ -19,19 +19,26 @@ def binary_image(image):
     binary = gray > threshold
     return binary > 0
 
+
+def im_dilate(im, gaussian_sigma=2):
+    im = gaussian(im, gaussian_sigma)
+    im = binary_image(im)
+    im = binary_dilation(im, disk(5))
+    return im
+
 def area_ecc_filter(im, area_bounds, ecc_bounds, ext_bounds=(0.0, 1.0)):
     # Extract the region props of the objects. 
     props = regionprops(im)
-    
+
     # Extract the areas and labels.
     areas = np.array([prop.area for prop in props])
     eccs = np.array([prop.eccentricity for prop in props])
     labels = np.array([prop.label for prop in props])
     extents = np.array([prop.extent for prop in props])
-    
+
     # Make an empty image to add the approved cells.
     im_approved = np.zeros_like(im)
-    
+
     # Threshold the objects based on area and eccentricity
     for i, _ in enumerate(areas):
         if areas[i] > area_bounds[0] and areas[i] < area_bounds[1]\
@@ -40,93 +47,103 @@ def area_ecc_filter(im, area_bounds, ecc_bounds, ext_bounds=(0.0, 1.0)):
                 im_approved += im==labels[i]
 
     return im_approved > 0
+
+
+class HeatmapWorker:
+    def __init__(self, inputFile, roadFile, gridFile, exteriorFile, outputFile):
+        self.image = imread(inputFile)
+        self.road_mask = imread(roadFile)
+        self.grid_mask = imread(gridFile)
+        self.exterior_mask = imread(exteriorFile)
+        self.output_file = outputFile
+
  
-def im_detect_lines(im, gaussian_sigma=2.5, small_object_size=1024):
-    im = gaussian(im, gaussian_sigma)
-    im = canny(im)
-    im = dilation(im)
-    im = remove_small_objects(im > 0.1, small_object_size)
-    return im
-
-def im_dilate(im, gaussian_sigma=2):
-    im = gaussian(im, gaussian_sigma)
-    im = binary_image(im)
-    im = binary_dilation(im, disk(5))
-    return im
-
-def im_detect_grid(im, dilation_selem=disk(2), line_length=500, line_gaps=30):
-    im = binary_image(im)
-    im = binary_dilation(im, dilation_selem)
-
-    lines = probabilistic_hough_line(im, line_length=line_length, line_gap=line_gaps)
-    grid_mask = np.zeros_like(im, dtype=np.bool)
-
-    for p0, p1 in lines:
-        # ignore diagonal lines
-        a = (p1[0]-p0[0]) / ((p1[1]-p0[1]) or float('nan'))
-        if abs(a) > 1e-1:
-            continue
-
-        rr, cc, val = line_aa(*p0[::-1], *p1[::-1])
-        grid_mask[rr, cc] = True
-
-    return grid_mask
-
-def im_detect_lines_area(lines, valid_area=50):
-    lines = 1-lines
-    im = -distance_transform_edt(lines)
-    return im > -valid_area
-
-def im_detect_blobs(im):
-    # im = rgb2gray(cv2.fastNlMeansDenoising(im))
-    im = rgb2gray(im)
-
-    # remove the grid from the image
-    mask_grid = im_detect_grid(im)
-    im[mask_grid] = im.max()
-
-    # detect lines
-    mask_lines = im_detect_lines(im, 2.3)
-    mask_available_area = im_detect_lines_area(mask_lines, 45)
-
-    # dilate and cut dilated blobs according to the previously detected bounding lines
-    im = im_dilate(im)
-    im &= mask_available_area
-    im &= ~mask_lines
-
-    # filter non-viable blobs
-    im = erosion(im, star(3))
-    im = area_ecc_filter(label(im), (50, 1000), (0., 1.0), (0.3, 1.0))
-
-    return im
-
-def im_remove_small_aligned_objects(im, line_length=200, line_gap=25, threshold_size=128):
-    # FIXME: - it'd be better to filter the image to have only small objects, 
-    #          so we can decrease `line_length`, and increase `line_gap`
-    # FIXME-END
-    im = im.copy() # that's slow and useless, but it avoids modifying the image given by reference
-
-    lines = probabilistic_hough_line(im, line_length=line_length, line_gap=line_gap)
-    mask_lines = np.zeros_like(im, dtype=np.bool)
-
-    # we draw a line joining the small objects of the image
-    for p0, p1 in lines:
-        rr, cc, val = line_aa(*p0[::-1], *p1[::-1])
-        mask_lines[rr, cc] = True
-
-    im[mask_lines] = 0
-    return remove_small_objects(im > 0, threshold_size)
+    def im_detect_lines(self, im, gaussian_sigma=2.5, small_object_size=1024):
+        im = gaussian(im, gaussian_sigma)
+        im = canny(im)
+        im = dilation(im)
+        im = remove_small_objects(im > 0.1, small_object_size)
+        return im
 
 
-def process_image(im, _road_mask):
-    im = im_detect_blobs(im)
-    im = im_remove_small_aligned_objects(im)
+    def im_detect_grid(self, dilation_selem=disk(2), line_length=500, line_gaps=30):
+        im = rgb2gray(self.image)
+        im = binary_image(im)
+        im = binary_dilation(im, dilation_selem)
 
-    return im
+        lines = probabilistic_hough_line(im, line_length=line_length, line_gap=line_gaps)
+        grid_mask = np.zeros_like(im, dtype=np.bool)
 
-def process_file(inputFile, roadFile, outputFile):
-    image = imread(inputFile)
-    road_mask = imread(roadFile)
-    result = process_image(image, road_mask)
+        for p0, p1 in lines:
+            # ignore diagonal lines
+            a = (p1[0]-p0[0]) / ((p1[1]-p0[1]) or float('nan'))
+            if abs(a) > 1e-1:
+                continue
 
-    imsave(outputFile, (result * 255).astype(np.uint8))
+            rr, cc, val = line_aa(*p0[::-1], *p1[::-1])
+            grid_mask[rr, cc] = True
+
+        return grid_mask
+
+
+    def im_detect_lines_area(self, lines, valid_area=50):
+        lines = 1-lines
+        im = -distance_transform_edt(lines)
+        return im > -valid_area
+
+
+    def im_detect_blobs(self, im):
+        im = rgb2gray(im)
+
+        # remove the grid from the image
+        # FIXME @Sami why recalculate the grid?
+        mask_grid = self.im_detect_grid()
+        im[mask_grid] = im.max()
+
+        # detect lines
+        mask_lines = self.im_detect_lines(im, 2.3)
+        mask_available_area = self.im_detect_lines_area(mask_lines, 45)
+
+        # dilate and cut dilated blobs according to the previously detected bounding lines
+        im = im_dilate(im)
+        im &= mask_available_area
+        im &= ~mask_lines
+
+        # filter non-viable blobs
+        im = erosion(im, star(3))
+        im = area_ecc_filter(label(im), (50, 1000), (0., 1.0), (0.3, 1.0))
+
+        return im
+
+
+    def im_remove_small_aligned_objects(self, im, line_length=200, line_gap=25, threshold_size=128):
+        # FIXME: - it'd be better to filter the image to have only small objects, 
+        #          so we can decrease `line_length`, and increase `line_gap`
+        # FIXME-END
+        im = im.copy() # that's slow and useless, but it avoids modifying the image given by reference
+
+        lines = probabilistic_hough_line(im, line_length=line_length, line_gap=line_gap)
+        mask_lines = np.zeros_like(im, dtype=np.bool)
+
+        # we draw a line joining the small objects of the image
+        for p0, p1 in lines:
+            rr, cc, val = line_aa(*p0[::-1], *p1[::-1])
+            mask_lines[rr, cc] = True
+
+        im[mask_lines] = 0
+        return remove_small_objects(im > 0, threshold_size)
+
+
+    def process_image(self):
+        im = self.im_detect_blobs(self.image)
+        im = self.im_remove_small_aligned_objects(im)
+
+        im = (im * 255).astype(np.uint8)
+        imsave(self.output_file, im, check_contrast=False)
+
+        return im
+
+
+def process_file(*kargs):
+    worker = HeatmapWorker(*kargs)
+    worker.process_image()
