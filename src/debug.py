53f95bf12e1c367ade70ee25a38fd0c637cd8cf9
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-07-05 14:00:01 +0200

    Add segment number to 04_debug segmentation

--- src/debug.py
+++ src/debug.py
@@ -56,6 +56,12 @@ def show_boxes(input_file, detection_directory, output_file):
         with annotation_path.open("r") as f:
             return json.load(f)["bbox"]
 
+    fonts_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fonts')
+    font_file = os.path.join(fonts_path, "Neufreit-ExtraBold.otf")
+    font = ImageFont.truetype(font_file, 16)
+    color_rgb = (0, 100, 150)
+    offset = (5, -30)
+
     img = Image.open(input_file)
     draw = ImageDraw.Draw(img)
 
@@ -63,5 +69,11 @@ def show_boxes(input_file, detection_directory, output_file):
         bbox = get_bbox(annot)
         box = [bbox[1], bbox[0], bbox[3], bbox[2]]
         draw.rectangle(box, outline=150)
+        draw.text(
+            (bbox[3] + offset[0], bbox[0] + offset[1]), # (x, y)
+            str(os.path.basename(annot)).split('.')[0],
+            color_rgb,
+            font=font
+        )
 
     img.save(output_file)
